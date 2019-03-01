6f0eb1edb89d57ec77119e7cc9ce5209b3e2e5b3
Shweta <shweta.rewatkar2@gmail.com>
2019-02-28 19:12:15 -0800

    add style

--- ch02/my-first-app/src/index.js
+++ ch02/my-first-app/src/index.js
@@ -14,9 +14,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
+
 const title = React.createElement(
     'h1',
-    {id: 'title', className: 'header'},
+    {id: 'title', className: 'header', style: style},
     'Hello World'
 )
 
