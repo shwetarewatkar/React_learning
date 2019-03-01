9503eebb0f48f3382d74cd60f7c4e8cf03e0fa0a
Shweta <shweta.rewatkar2@gmail.com>
2019-02-28 20:05:38 -0800

    Update index.js

--- ch02/my-first-app/src/index.js
+++ ch02/my-first-app/src/index.js
@@ -21,18 +21,16 @@ let skiData ={
     goal: 100
 }
 
-//component creation
-class SkiDayCounter extends Component{
-    getPercent = decimal => {
-        return decimal * 100 + '%'
-    }
-    calcGoalProgress = (total,goal) =>{
-        return this.getPercent(total/goal)
-    }
-    render(){
-        const {total,powder,backcountry,goal} = this.props
-        return(
-            <section>
+const getPercent = decimal => {
+    return decimal * 100 + '%'
+}
+const calcGoalProgress = (total,goal) =>{
+    return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total,powder,backcountry,goal}) =>{
+    return(
+        <section>
                 <div>
                     <p>Total Days ={total}</p>
                 </div>
@@ -43,12 +41,12 @@ class SkiDayCounter extends Component{
                     <p>Backcountry Days ={backcountry}</p>
                 </div>
                 <div>
-                    <p>Goal Progress={this.calcGoalProgress(total,goal)}</p>
+                    <p>Goal Progress={calcGoalProgress(total,goal)}</p>
                 </div>
             </section>
-        )
-    }
+    )
 }
+
 
 render(
     //making dynamic using React props
